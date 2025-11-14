140f1f279d55b690dba74574d51df13744edc77d
nevena <dnevena27@gmail.com>
2025-11-14 11:07:47 +0200

    WP:UI Components - Lists and Dropdowns

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -5,13 +5,148 @@ test.beforeEach(async ({ page }) => {
   await page.goto("https://testautomationpractice.blogspot.com/");
 });
 
-test.describe("Form Layouts page", async () => {
-  test("input fields", async ({ page }) => {
+test.describe("Form Layouts page - GUI Elements", async () => {
+  test("input field Name", async ({ page }) => {
     const usingTheNameInput = page
       .locator(".form-group")
       .nth(0)
       .getByRole("textbox", { name: "Name" });
 
     await usingTheNameInput.fill("Jane Doe");
+    await usingTheNameInput.clear();
+  });
+
+  test("input field Email", async ({ page }) => {
+    const usingTheEmailInput = page
+      .locator(".form-group")
+      .getByPlaceholder("Enter EMail");
+
+    await usingTheEmailInput.fill("test@test.com");
+    await usingTheEmailInput.clear();
+    await usingTheEmailInput.pressSequentially("test@test.com", { delay: 500 });
+
+    //generic assertion
+    const inputValue = await usingTheEmailInput.inputValue();
+    expect(inputValue).toEqual("test@test.com");
+
+    //locator assertion
+    await expect(usingTheEmailInput).toHaveValue("test@test.com");
+  });
+
+  test("input field Phone", async ({ page }) => {
+    const usingThePhoneInput = page
+      .locator(".form-group")
+      .getByRole("textbox", { name: "phone" });
+    await usingThePhoneInput.fill("+35988888888");
+    await usingThePhoneInput.clear();
+  });
+
+  test("input field Address", async ({ page }) => {
+    const usingTheAddressInput = page
+      .locator(".form-group")
+      .getByLabel("Address:");
+    await usingTheAddressInput.fill("BG, Varna");
+    await usingTheAddressInput.clear();
+  });
+
+  test("radio buttons", async ({ page }) => {
+    const usingGUIElements = page.locator(".form-group");
+
+    //await usingGUIElements.getByLabel("Female").check();
+
+    // await usingGUIElements
+    //   .locator('input[name="gender"][value="female"]')
+    //   .check();
+
+    await usingGUIElements.getByRole("radio", { name: "female" }).check();
+    const radioButtonFemale = usingGUIElements.getByRole("radio", {
+      name: "female",
+    });
+    expect(radioButtonFemale).toBeChecked();
+
+    await usingGUIElements
+      .locator('input[name="gender"][value="male"]')
+      .check();
+    const radioButtonMale = usingGUIElements.locator(
+      'input[name="gender"][value="male"]'
+    );
+    expect(await radioButtonFemale.isChecked()).toBeFalsy();
+    expect(await radioButtonMale.isChecked()).toBeTruthy();
+  });
+
+  test("checkboxes", async ({ page }) => {
+    await page.locator(".form-group");
+
+    await page.getByRole("checkbox", { name: "Sunday" }).click();
+    await page.getByRole("checkbox", { name: "Monday" }).check();
+    await page.getByRole("checkbox", { name: "Saturday" }).click();
+
+    expect(
+      await page.getByRole("checkbox", { name: "Sunday" }).isChecked()
+    ).toBeTruthy();
+    expect(
+      await page.getByRole("checkbox", { name: "Monday" }).isChecked()
+    ).toBeTruthy();
+    expect(
+      await page.getByRole("checkbox", { name: "Saturday" }).isChecked()
+    ).toBeTruthy();
+    expect(
+      await page.getByRole("checkbox", { name: "Tuesday" }).isChecked()
+    ).toBeFalsy();
+    expect(
+      await page.getByRole("checkbox", { name: "Wednesday" }).isChecked()
+    ).toBeFalsy();
+    expect(
+      await page.getByRole("checkbox", { name: "Thursday" }).isChecked()
+    ).toBeFalsy();
+    expect(
+      await page.getByRole("checkbox", { name: "Friday" }).isChecked()
+    ).toBeFalsy();
+
+    const allBoxes = page.locator(".form-group").nth(3).getByRole("checkbox");
+    for (const box of await allBoxes.all()) {
+      await box.check();
+      expect(await box.isChecked).toBeTruthy();
+    }
+  });
+
+  test("lists and dropdowns", async ({ page }) => {
+    const dropdownMenu = page.locator("select[id='country']");
+    await dropdownMenu.click();
+
+    const optionCountryList = page.locator("select[id='country'] option");
+    await expect(optionCountryList).toHaveCount(10);
+
+    await expect(optionCountryList.filter({ hasText: "France" })).toHaveCount(
+      1
+    );
+
+    await dropdownMenu.selectOption({ label: "Brazil" });
+    await expect(dropdownMenu).toHaveValue("brazil");
+
+    const countries = {
+      usa: "United States",
+      canada: "Canada",
+      uk: "United Kingdom",
+      germany: "Germany",
+      france: "France",
+      australia: "Australia",
+      japan: "Japan",
+      china: "China",
+      brazil: "Brazil",
+      india: "India",
+    };
+
+    await dropdownMenu.click();
+    for (const country in countries) {
+      await dropdownMenu.selectOption({ label: countries[country] });
+      const selectedOption = page.locator(
+        "select[id='country'] option:checked"
+      );
+      await expect(selectedOption).toHaveText(countries[country]);
+      if (country != "india") {
+        await dropdownMenu.click();
+      }
+    }
   });
 });
